bde9fd0ce8b11e164896056d7cd69b5ff915b825
ChiaraFerdynus <70207153+ChiaraFerdynus@users.noreply.github.com>
2020-09-28 11:25:00 +0100

    Update FinalData.py

--- FinalData.py
+++ FinalData.py
@@ -14,7 +14,7 @@ OW = pd.read_excel("NewFormat_OW.xlsx")
 
 columns = OW.columns[1:]  # drop first one (bc it is the Year)
 
-
+# determine percentage of missing values
 def missing_values(snip, window, threshold):
     missing = snip.isnull().sum()
     percent = (missing/window)*100
@@ -25,6 +25,7 @@ def missing_values(snip, window, threshold):
         return 1
 
 
+# determine Q value for single location
 def mk_col_slope(Data, YearCol, LocCol, window, TH, alpha=0.1):
     length = Data[LocCol].shape[0]
     start_index = Data[YearCol].values[window - 1]
@@ -105,14 +106,14 @@ def mk_col_z(Data, YearCol, LocCol, window, TH, alpha=0.1):
     # print(stats_list2)
     return stats_list2
 
-
+# create complete data frame of Q values for all locations
 def merge_frame_slope(data, window, threshold):
     sample_list = pd.DataFrame(index=range(0, data.shape[0] - (window - 1)))
     year = pd.Series(data['YEAR'][0+(window - 1):])
     year.reset_index(drop=True, inplace=True)
     sample_list.insert(0, 'Year', year)
 
-    for entry in columns:
+    for entry in columns:       # iterating over the locations
         prev = mk_col_slope(data, 'YEAR', entry, window, threshold)
         sample_list = pd.merge(sample_list, prev, on='Year', how='outer')
 
@@ -163,6 +164,7 @@ def merge_frame_Z(data, window, threshold):
 
 # 50 year window 25% tolerance
 
+# --> this is how data frames from python can be stored in Excel
 '''
 writer = pd.ExcelWriter('Z_FU_50w_25md.xlsx', engine='xlsxwriter')
 workbook = writer.book
